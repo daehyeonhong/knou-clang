7825ac1fdc1d185859fe9b087dacc86b5aa6a0ca
DaeHyeonHong <fholng@outlook.com>
2022-04-26 01:09:54 +0900

    매크로

---
+++ second/macro/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define ADDR "서울특별시 종로구 대학로 86(동숭동)"
+#define TEL"1577-9995"
+
+void main() {
+    printf("한국방송통신대학교 주소:%s\n", ADDR);
+    printf("한국방송통신대학교 전화번호:%s\n", TEL);
+}
