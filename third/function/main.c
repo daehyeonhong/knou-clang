3df400dd4b98ddaf34cc620ec847a606ee0a6180
hongdae <fholng@outlook.com>
2022-04-27 22:26:10 +0900

    [3] 함수

---
+++ third/function/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char c = 'A';
+    int i = 10, j = 20, k = 30;
+    printf("간단한 출력 program\n");
+    printf("c=%c, c의 아스키 코드 값은 %d\n", c, c);
+    printf("i=%d, j=%d, k=%d", i, j, k);
+}
