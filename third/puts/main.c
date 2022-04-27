24b4dc3fc9ce6e678ccdb1f95670713e72327dfa
hongdae <fholng@outlook.com>
2022-04-27 23:51:09 +0900

    puts() vs printf()

---
+++ third/puts/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char s1[] = "Computer";
+    char s2[] = "Science";
+    puts(s1);
+    puts(s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    return 0;
+}
