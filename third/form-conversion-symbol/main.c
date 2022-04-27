d0d410a974078a0804566f03d3d6893cfa3a8039
hongdae <fholng@outlook.com>
2022-04-27 23:12:05 +0900

    출력함수

---
+++ third/form-conversion-symbol/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("%c\n", 'a');
+    printf("%d\n", -123);
+    printf("%o\n", 123);
+    printf("%x\n", 123);
+    printf("%X\n", 123);
+    printf("%f\n", 123.456789);
+    printf("%e\n", 123.456789);
+    printf("%s\n", "abcdefg");
+    return 0;
+}
