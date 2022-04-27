896c1c90a926c8ef5436a67610f32e00f6e73c27
hongdae <fholng@outlook.com>
2022-04-27 23:32:15 +0900

    char input function

--- third/input-function/main.c
+++ third/input-function/main.c
@@ -3,13 +3,9 @@
 #pragma warning(disable:4996)
 
 int main() {
-    int jsu1, jsu2;
-    float ssu1, ssu2;
-    printf("\n정수를 입력하시오");
-    scanf("%d %d", &jsu1, &jsu2);
-    printf("\n실수를 입력하세요");
-    scanf("%f %f", &ssu1, &ssu2);
-    printf("\n정수는 %d %d \n", jsu1, jsu2);
-    printf("실수는 %f %f\n", ssu1, ssu2);
+    char a;
+    printf("문자 하나를 입력하세요 \n");
+    a = getchar();
+    printf("a=%c\n", a);
     return 0;
 }
