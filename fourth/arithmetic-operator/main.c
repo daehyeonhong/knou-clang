709349baa02b078d7fe8ae9e16ea849024d24cf9
hongdae <fholng@outlook.com>
2022-04-28 22:47:09 +0900

    arthmetic-operator

---
+++ fourth/arithmetic-operator/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 3;
+    printf("x+y=%d\n", x + y);
+    printf("x/y=%d\n", x / y);
+    printf("x%%y=%d\n", x % y);
+    printf("y%%d%d\n", y % x);
+}
