db1aaf1b31883f43bcbc573b5bbd2277855e1ea4
hongdae <fholng@outlook.com>
2022-04-28 22:52:34 +0900

    relational-operator

---
+++ fourth/relational-operator/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 4;
+    int b = a > 2;
+    printf("b=%d\n", b);
+    int c = a < 2;
+    printf("c=%d\n", c);
+    int d = a == 4;
+    printf("d=%d\n", d);
+    return 0;
+}
