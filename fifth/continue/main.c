620197d80288e51ec46851c7e46535564394d250
hongdae <fholng@outlook.com>
2022-05-01 12:39:20 +0900

    a

--- fifth/continue/main.c
+++ fifth/continue/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <math.h>
+
+#pragma warning(disabled:4996)
 
 int main() {
-    printf("Hello, World!\n");
+    int num = 1;
+    while (num != 0) {
+        printf("\n num=?");
+        scanf("%d", &num);
+        if (num < 0) {
+            printf("0:Negative Number!!!\n");
+            continue;
+        }
+        printf("Square root of %d=%f\n", num, sqrt(num));
+    }
+    printf("\n\n The end");
     return 0;
 }
