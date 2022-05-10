610981771d4d4536ed3e2bfc2cfd877071b07e17
daehyeonhong <fholng@outlook.com>
2022-05-10 07:09:32 -0700

    출석 수업 과제1-1

---
+++ attendance/1-1/Hello.c
@@ -0,0 +1,9 @@
+/*  예제: Hello 출력
+ *  파일 이름: Hello.c
+ *  수정 일자: 2022년 5월 10일
+ *  작성자: 홍대현
+*/
+#include <stdio.h>      // 모니터 출력 함수 printf 사용을 위해 헤더 파일 추가
+void main() {           // main 함수 시작
+    printf("Hello~\n"); // 문자열 출력
+}                       // main 종료
