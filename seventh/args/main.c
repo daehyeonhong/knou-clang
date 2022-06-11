d1bdab1e8f8450e1d9ae2db7477870756b2b985f
DaeHyeonHong <fholng@outlook.com>
2022-06-11 20:52:52 +0900

    매개변수 참조 전달

--- seventh/args/main.c
+++ seventh/args/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-void swap(int x, int y);
+void swap(int *x, int *y);
 
 void main() {
     int a = 3, b = 5;
     printf("before call a=%d, b=%d\n", a, b);
-    swap(a, b);
+    swap(&a, &b);
     printf("after call a=%d, b=%d\n", a, b);
 }
 
-void swap(int x, int y) {
+void swap(int *x, int *y) {
     int temp;
-    temp = x;
-    x = y;
-    y = temp;
-    printf("in function x=%d, y=%d\n", x, y);
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    printf("in function x=%d, y=%d\n", *x, *y);
 }
