4df88188b9c948794207952b8a1749c935088e80
DaeHyeonHong <fholng@outlook.com>
2022-06-16 00:00:44 +0900

    edit

--- eighth/array-pointer/main.c
+++ eighth/array-pointer/main.c
@@ -7,15 +7,15 @@ void main() {
     int array4[4] = {0};
     int i;
     for (i = 0; i <= 3; ++i)
-        printf("array1[%d] = %d ]t", i, array1[i]);
+        printf("array1[%d] = %d \t", i, array1[i]);
     printf("\n");
     for (i = 0; i <= 3; ++i)
-        printf("array2[%d] = %d ]t", i, array2[i]);
+        printf("array2[%d] = %d \t", i, array2[i]);
     printf("\n");
     for (i = 0; i <= 3; ++i)
-        printf("array3[%d] = %d ]t", i, array3[i]);
+        printf("array3[%d] = %d \t", i, array3[i]);
     printf("\n");
     for (i = 0; i <= 3; ++i)
-        printf("array4[%d] = %d ]t", i, array4[i]);
+        printf("array4[%d] = %d \t", i, array4[i]);
     printf("\n");
 }
