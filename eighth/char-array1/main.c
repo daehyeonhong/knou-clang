e63fa343b9ca70775228ec45f34d7c37ad7562b5
DaeHyeonHong <fholng@outlook.com>
2022-06-18 16:02:27 +0900

    char array

---
+++ eighth/char-array1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+    char name[] = "HONG GIL DONG";
+    char adrs1[6] = {'S', 'E', 'O', 'U', 'L', 0};
+    char adrs2[6]={'S','E','O','U','L'};
+    printf("\n name: %s", name);
+    printf("\n adrs1: %s", adrs1);
+    printf("\n adrs2: %s", adrs2);
+}
