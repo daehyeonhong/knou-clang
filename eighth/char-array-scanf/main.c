81496c87f802b82911336abefc1ae55805367954
DaeHyeonHong <fholng@outlook.com>
2022-06-18 16:02:05 +0900

    char array sacnf

---
+++ eighth/char-array-scanf/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+  char string[50];
+  int i =0;
+    printf("문자열을 입력하세요: ");
+    scanf("%s", string);
+    printf("입력 받은 문자열: %s\n", string);
+    printf("문자 단위 출력: ");
+    while (string[i] != '\0') printf("%c", string[i++]);
+}
