cf40b24f08d14229eaaa978feccec915d29dca4a
HongDaeHyeon <fholng@outlook.com>
2022-11-19 14:43:55 +0900

    pointer delcaration and definition

--- data-structure/linked-list/main.c
+++ data-structure/linked-list/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <malloc.h>
 
 struct linked_list_node {
     int data;
@@ -6,6 +7,18 @@ struct linked_list_node {
 };
 
 int main() {
-    printf("Hello, World!\n");
+    int a, *p_a;
+    float b, *p_b;
+
+    p_a = (int *) malloc(sizeof(int));
+    p_b = (float *) malloc(sizeof(float));
+
+    *p_a = 10;
+    *p_b = 3.14f;
+
+    printf("a is %d, b is %f\n", *p_a, *p_b);
+    free(p_a);
+    free(p_b);
+    printf("a is %d, b is %f\n", *p_a, *p_b);
     return 0;
 }
