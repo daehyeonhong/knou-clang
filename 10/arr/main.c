502d8658cb791534d5066cd7749a07d65f1024c8
HongDaeHyeon <fholng@outlook.com>
2022-11-21 19:18:41 +0900

    int array

---
+++ 10/arr/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    static int a[] = {10, 20, 30, 40, 50};
+    int *pt, b, c, d;
+    pt = a;
+    b = *pt + *(pt + 3);
+    pt++;
+    c = *pt + *(pt + 3);
+    d = *pt + 3;
+    printf("b=%d, c=%d, d=%d", b, d, c);
+    return 0;
+}
