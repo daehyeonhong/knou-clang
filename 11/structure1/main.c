bb16d0fe7a1ad1611de4a131a60bb7fa94dbdec5
HongDaeHyeon <fholng@outlook.com>
2022-11-24 23:58:28 +0900

    base structure'

---
+++ 11/structure1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct score {
+    char no[4];
+    char name[8];
+    int kor;
+    int eng;
+    int math;
+    int hist;
+};
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
