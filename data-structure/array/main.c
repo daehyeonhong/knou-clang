73ba6bc1e53d0c05c174b8245c3e51f861c88ec7
DaeHyeonHong <fholng@outlook.com>
2022-11-26 22:58:13 +0900

    store

--- data-structure/array/main.c
+++ data-structure/array/main.c
@@ -16,6 +16,12 @@ int retrieve(int *a, int i) {
     return (-1);
 }
 
+void store(int *a, int i, int e) {
+    if (i >= 0 && i < array_size)
+        a[i] = e;
+    else printf("Error\n");
+}
+
 int main() {
     printf("Hello, World!\n");
     return 0;
