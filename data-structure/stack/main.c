40fc9827a13d93c7dfd2d923e73fba757f866347
DaeHyeonHong <fholng@outlook.com>
2022-11-26 23:13:26 +0900

    pop

--- data-structure/stack/main.c
+++ data-structure/stack/main.c
@@ -5,6 +5,13 @@ typedef int element;
 element stack[STACK_SIZE];
 int top = -1;
 
+element pop() {
+    if (top == -1) {
+        printf("Stack is empty!!\n");
+        return 0;
+    }
+    return stack[top--];
+}
 
 int main() {
     printf("Hello, World!\n");
