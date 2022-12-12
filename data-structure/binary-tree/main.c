baeee85ed09bbe93168ed5a70cd3157f415907c0
HongDaeHyeon <fholng@outlook.com>
2022-12-12 21:52:07 +0900

    PRL(전위순회)

--- data-structure/binary-tree/main.c
+++ data-structure/binary-tree/main.c
@@ -6,6 +6,16 @@ struct node {
     int info;
 };
 
+struct node *nodeptr;
+
+void preorder(struct node *tree_ptr) {
+    if (tree_ptr) {
+        printf("%d", tree_ptr->info);
+        preorder(tree_ptr->left);
+        preorder(tree_ptr->right)
+    }
+}
+
 int main() {
     printf("Hello, World!\n");
     return 0;
