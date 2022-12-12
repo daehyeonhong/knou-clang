be5bc3760a0ecb5143239c15d3236c7b4ac691dc
HongDaeHyeon <fholng@outlook.com>
2022-12-12 21:57:31 +0900

    노드개수세기

--- data-structure/binary-tree/main.c
+++ data-structure/binary-tree/main.c
@@ -12,11 +12,20 @@ void preorder(struct node *tree_ptr) {
     if (tree_ptr) {
         printf("%d", tree_ptr->info);
         preorder(tree_ptr->left);
-        preorder(tree_ptr->right)
+        preorder(tree_ptr->right);
     }
 }
+
 
 int main() {
     printf("Hello, World!\n");
     return 0;
+}
+
+int get_node_count(nodeptr *root) {
+    if (root == null) return 0;
+    int result = 1;
+    result = get_node_count((nodeptr*)root->left)
+           + get_node_count((nodeptr*)root->right);
+    return result;
 }
