1eabb07c3408325a5afada21dc980caf384756bc
DaeHyeonHong <fholng@outlook.com>
2022-12-14 23:36:42 +0900

    insert

--- data-structure/bs-tree/main.c
+++ data-structure/bs-tree/main.c
@@ -23,6 +23,14 @@ struct KNode *Search(char k[], struct KNode *r) {
     else return Search(k, r->right);
 }
 
+struct KNode *Insert(struct KNode *newptr, struct KNode *r) {
+    if (r == NULL)return newptr;
+    else if (strcmp(newptr->key, r->key) == 0)DUPLICATE_ENTRY();
+    else if (strcmp(newptr->key, r->key) < 0) r->left = Insert(newptr, r->left);
+    else r->right = Insert(newptr, r->right);
+    return r;
+}
+
 int main() {
     printf("Hello, World!\n");
     return 0;
