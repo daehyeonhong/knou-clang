d0314c23a47caa3614b99d0fe8acb6352cba4da2
DaeHyeonHong <fholng@outlook.com>
2022-12-14 23:00:34 +0900

    bs-tree inorder

--- data-structure/bs-tree/main.c
+++ data-structure/bs-tree/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+struct KNode {
+    struct KNode *left;
+    char key[10];
+    int info;
+    struct KNode *right;
+};
+
+void Inorder(struct KNode *rootptr) {
+    if (rootptr != NULL) {
+        Inorder(rootptr->left);
+        printf("%d", rootptr->info);
+        Inorder(rootptr->right);
+    }
+}
+
 int main() {
     printf("Hello, World!\n");
     return 0;
